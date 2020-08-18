4214465811fc082ce3b68fb6de805f88497f7e29
zoomrcd12fa <63201132+zoomrcd12fa@users.noreply.github.com>
2020-08-18 12:06:11 +0200

    insert OperationView in routes

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { Route } from 'react-router-dom';
 import BarrelSetView from './views/BarrelSetView';
 import BarrelView from './views/BarrelView';
 import OperationTypeView from './views/OperationTypeView';
+import OperationView from './views/OperationView';
 
 const BaseRouter = () => (
   <div>
     <Route exact path='/barrel_set' component={BarrelSetView} />
     <Route exact path='/barrel_set/:setID' component={BarrelView} />
     <Route exact path='/operation_type' component={OperationTypeView} />
+    <Route exact path='/operation/:name' component={OperationView} />
   </div>
 );
 

6abe4b42fe76fbe569041d73669005f04bb7ce25
zoomrcd12fa <63201132+zoomrcd12fa@users.noreply.github.com>
2020-08-20 11:37:59 +0200

    delete useless requests methods

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,54 +1,5 @@
 const BASE_URL = "http://localhost:8000/api"
 
-export const get = async (url: string) => {
-	return fetch(`${BASE_URL}/${url}`, {
-	    method: 'GET',
-	    headers: {
-        'Content-Type': 'application/json',
-    	   Accept: 'application/json',
-    	}
-  })
-  .then(res => {
-    if (!res.ok){
-      throw new Error(`Error with status ${res.status}`);
-    }
-    return res.json();
-  })
-};
-
-
-export const post = async (url: string, body: Object) => {
-  return fetch(`${BASE_URL}/${url}`, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    }
-  }).then(res => {
-    if (!res.ok) {
-      throw new Error(`Error with status ${res.status}`);
-    }
-    return res.json();
-  });
-};
-
-export const put = async (url: string, body: Object) => {
-	return fetch(`${BASE_URL}/${url}`, {
-		method: 'PUT',
-		body: JSON.stringify(body),
-		headers: {
-  		'Content-Type': 'application/json',
-  		Accept: 'application/json'
-  	}
-	}).then(async res => {
-    if (!res.ok) {
-      throw new Error(`Error with status ${res.status}`);
-    }
-		return res.json();
-	});
-};
-
 export const request = async (url: string, method:string, body: Object) => {
 	return fetch(`${BASE_URL}/${url}`, {
 		method: method,

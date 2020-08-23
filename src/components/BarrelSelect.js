dcd42f414a13da1c604126ec95d593da23cb41f7
zoomrcd12fa <63201132+zoomrcd12fa@users.noreply.github.com>
2020-08-23 17:48:24 +0200

    barrelSelect changes

--- src/components/BarrelSelect.js
+++ src/components/BarrelSelect.js
@@ -1,27 +1,81 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 
 import { request } from '../helpers/requests';
 
 class BarrelSelect extends React.Component {
   state = {
-    barrels: []
+    sets: [],
+    barrels: [],
+    selectedSet: null
   }
 
-  async componentDidMount(){
-    this.setState({ barrels: await request("barrel/", 'GET')});
+  onSelectedSet = e => {
+    this.setState({selectedSet: Number(e.target.value)});
+  }
+
+  async componentDidMount (){
+    this.setState({
+      sets: await request("barrel_set/", 'GET'),
+      barrels: await request("barrel/", 'GET')
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+
+    if (this.state.selectedSet === null &&
+        this.state.barrels.length !== 0 &&
+        this.props.value !== undefined) {
+          console.log(this.props.value);
+          const item = this.state.barrels.filter(barrel => barrel.id === this.props.value)[0]
+          console.log(item);
+          this.setState({selectedSet: item.barrel_set});
+    }
   }
 
   render() {
     return (
-      <Form.Control as="select"
-        name={this.props.name}
-        value={this.props.value}
-        onChange={e => this.props.onChange(e)}>
-          {this.state.barrels.map(barrel => {
-            return (<option key={barrel.id} value={barrel.id}>{barrel.id}</option>)
-          })}
-      </Form.Control>
+      <Form.Row>
+        <Col>
+          <Form.Control as="select"
+                        value = {this.state.selected_set}
+                        defaultValue=""
+                        onChange = {this.onSelectedSet}
+                        required>
+                        { !this.props.value ?
+                          (<option disabled value=""> seleziona una batteria</option>)
+                          : (null)
+                        }
+                        {this.state.sets.map((set, i) => {
+                            if(set.id === this.state.selectedSet)
+                              return (<option key={i} selected value={set.id}>{set.id}</option>)
+                            return (<option key={i} value={set.id}>{set.id}</option>)
+                        })}
+          </Form.Control>
+        </Col>
+        <Col>
+          <Form.Control as="select"
+                        name={this.props.name}
+                        value={this.props.value}
+                        defaultValue=""
+                        onChange={this.props.onChange}
+                        disabled = {this.state.selectedSet === null}
+                        required >
+                          { !this.props.value ?
+                            (<option disabled value=""> seleziona un barile</option>)
+                            : (null)
+                          }
+                          {
+                            this.state.barrels.filter(barrel => {
+                              return barrel.barrel_set === this.state.selectedSet;
+                            }).map((barrel, i) => {
+                              return (<option key={i} value={barrel.id}>{barrel.id}</option>)
+                            })
+                          }
+          </Form.Control>
+        </Col>
+      </Form.Row>
     )
   }
 }

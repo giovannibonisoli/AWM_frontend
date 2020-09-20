137f1c0f8600a105e77b128a920f05ab6c598198
zoomrcd12fa <63201132+zoomrcd12fa@users.noreply.github.com>
2020-09-20 23:34:05 +0200

    Insert Search function

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react'
 import Table  from 'react-bootstrap/Table';
 import Button  from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Highlighter from "react-highlight-words";
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { Link } from 'react-router-dom';
+import { Search } from 'react-bootstrap-icons';
 
 import ModalForm from './ModalForm';
 import EliminationModal from './EliminationModal';
 
 class DataTable extends Component {
   state = {
-    page: 0
+    page: 0,
+    search: ''
   }
+
+  setSearch = e => {
+    this.setState({ search: e.target.value });
+  };
+
 
   activeButton = (index) => {
     return this.state.page === index;
@@ -37,14 +48,48 @@ class DataTable extends Component {
     const itemsLength = this.props.items.length;
     const groupLength = Math.ceil((itemsLength/5))
     const maxsize = 3;
-    return (
-      <div >
-        <ModalForm title = {`Aggiungi "${this.props.objectName}"`}
-                        buttonInfo = {[`+ Aggiungi`, "success", "float-xl-right"]}
-                        action = {this.props.addAction}
-                        fields = {this.props.fields}
-                        variable={this.props.variable} />
+    const filteredItems = this.props.items.filter(item => {
+      if(this.state.search !== ""){
 
+        for(let field of this.props.fields) {
+          if(item[field.field] !== undefined){
+            let thing = item[field.field];
+            if (typeof thing === "number"){
+              thing = thing.toString();
+            }
+            const isin = thing.toLowerCase().includes(this.state.search.toLowerCase());
+            if (isin !== false){
+              return true;
+            }
+          }
+        }
+        return false;
+      }
+      return true;
+    });
+    return (
+      <div>
+        <Form.Row>
+          <Col sm="2">
+            <InputGroup className="mb-2">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1"><Search size={14}/></InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                placeholder="Cerca..."
+                onChange={this.setSearch}
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Col>
+          <Col>
+            <ModalForm title = {`Aggiungi "${this.props.objectName}"`}
+                            buttonInfo = {[`+ Aggiungi`, "success", "float-xl-right"]}
+                            action = {this.props.addAction}
+                            fields = {this.props.fields}
+                            variable={this.props.variable} />
+          </Col>
+        </Form.Row>
         <Table style={{marginTop: 20}} responsive bordered hover>
           <thead>
             <tr>
@@ -53,34 +98,42 @@ class DataTable extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.props.items.slice(page*5, (page+1)*5).map(item => {
-              return (
-                <tr key={item.id}>
-                  {this.props.fields.map((field, i) => <td key={i}>{item[field.field]}</td>)}
-                  <td>
-                    <div>
-                      <ModalForm title = {`Modifica ${this.props.objectName}`}
-                                  buttonInfo={["Modifica", "primary", ""]}
-                                  item = {item}
-                                  action = {this.props.updateAction}
-                                  fields = {this.props.fields}
-                                  variable={this.props.variable}/>
-                      <EliminationModal objectName = {this.props.objectName}
-                                        item = {item}
-                                        deleteAction = {this.props.deleteAction}/>
-                      {
-                        this.props.detailed ?
-                        <Link to={`${this.props.detailed[0]}/${item.id}`}>
-                              <Button variant="info">{this.props.detailed[1]}</Button>
-                        </Link>
-                        :
-                        <div></div>
-                      }
-                    </div>
-                  </td>
-                </tr>
-              )
-            })}
+            {filteredItems.slice(page*5, (page+1)*5).map(item => (
+              <tr key={item.id}>
+                {this.props.fields.map((field, i) => {
+                  let thing = item[field.field];
+                  if (typeof thing === "number"){
+                    thing = thing.toString();
+                  }
+                  return (<td key={i}>
+                    <Highlighter
+                        highlightClassName="YourHighlightClass"
+                        searchWords={[this.state.search]}
+                        autoEscape={true}
+                        textToHighlight={thing}
+                      />
+                  </td>)
+                })}
+                <td>
+                  <div>
+                    <ModalForm title = {`Modifica ${this.props.objectName}`}
+                                buttonInfo={["Modifica", "primary", ""]}
+                                item = {item}
+                                action = {this.props.updateAction}
+                                fields = {this.props.fields}
+                                variable={this.props.variable}/>
+                    <EliminationModal objectName = {this.props.objectName}
+                                      item = {item}
+                                      deleteAction = {this.props.deleteAction}/>
+                    {this.props.detailed &&
+                      <Link to={`${this.props.detailed[0]}/${item.id}`}>
+                            <Button variant="info">{this.props.detailed[1]}</Button>
+                      </Link>}
+                  </div>
+                </td>
+              </tr>
+            )
+          )}
           </tbody>
         </Table>
 
